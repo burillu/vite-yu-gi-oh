199626130744d47b31819a6cb75c26af66868aa5
burillu <burillu90@hotmail.it>
2023-11-13 20:42:49 +0100

    scaffolding and content readme

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h1>{{ nome }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+
+  data() {
+    return {
+      nome: 'pippo',
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped></style>

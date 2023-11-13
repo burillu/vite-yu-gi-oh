d53ee6d6fb5d4a387ba27f85e88dec1aaea751fa
burillu <burillu90@hotmail.it>
2023-11-13 21:12:28 +0100

    add AppCard component

---
+++ src/components/AppCard.vue
@@ -0,0 +1,21 @@
+<template>
+    <div :id="id">
+        <img :src="src" :alt="name">
+        <div>
+            <span>{{ name }}</span>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'AppCard',
+    props: {
+        src: String,
+        name: String,
+        id: Number,
+    }
+}
+</script>
+
+<style lang="scss" scoped></style>
